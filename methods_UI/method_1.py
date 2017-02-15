bfe50376359417549286cd210ff86e8b058ae7b0
MeiZhangjun <eagle18335@163.com>
2017-02-15 09:57:31 +0800

    method_1 update
    change functions name
    make it more convenient to print several lines

--- methods_UI/method_1.py
+++ methods_UI/method_1.py
@@ -1,23 +1,35 @@
+# method_1 includes thoes methods about drawing lines
 import matplotlib.pyplot as plt
 import numpy as np
 
-def draw_line(P, X):
+def resetPlot():
+
+    X = np.linspace(0, 240, 240, endpoint = True)
+
     # Create new figure
     plt.figure(figsize = (12,6), dpi = 80)
     plt.subplot(111)
 
-    #Plot
-    plt.plot(X, P, color = "black", linewidth = 1.0, linestyle = "-")
-
     # set limits, ticks
     plt.xlim(X.min() - 10, X.max() + 10)
-    plt.ylim(P.min(), P.max())
+    # plt.ylim(P.min(), P.max())
 
     plt.xticks([0,30,60,90,120,150,180,210,240])
 
     # show it
+    # plt.show()
+
+def drawLine(P):
+    X = np.linspace(0, 240, 240, endpoint = True)
+    plt.plot(X, P)
+
+def drawAll():
     plt.show()
 
-# Create base line
+# #For testing
+#
+# #Create base line
 X = np.linspace(0, 240, 240, endpoint = True)
-P = np.random.random_sample(240) * 10 + 5
+P = np.random.random_sample(240) * 10 + 15
+P1 = np.random.random_sample(240) * 10 + 5
+P2 = np.random.random_sample(240) * 20 + 5
